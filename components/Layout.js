2ef72102b48b19a7d7ddf79c4ddf000b8d5c9b0c
a9817560 <a9817560@gmail.com>
2021-06-08 01:31:04 +0800

    Add<Showcase>with images placed in /public/images

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Header from './Header';
+import Showcase from './Showcase';
 import Footer from './Footer';
 import styles from './Layout.module.css';
 export default function Layout({ title, description, keyword, children }) {
@@ -10,6 +12,7 @@ export default function Layout({ title, description, keyword, children }) {
         <meta name={keyword} content={description} />
       </Head>
       <Header />
+      <Showcase />
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
