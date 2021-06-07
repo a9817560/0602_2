d756193b55e8d13e3d3c3621ef37213003f42ba3
a9817560 <a9817560@gmail.com>
2021-06-08 01:13:07 +0800

    Add module aliases

--- components/Layout.js
+++ components/Layout.js
@@ -2,16 +2,16 @@ import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
 import styles from './Layout.module.css';
-
-export default function Layout({ title, description, keywords, children }) {
+export default function Layout({ title, description, keyword, children }) {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta name='description' content={description} />
-        <meta name='keywords' content={keywords} />
+        <meta name={keyword} content={description} />
       </Head>
-      {children}
+      <Header />
+      <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
